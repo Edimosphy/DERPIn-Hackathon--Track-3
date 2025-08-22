f0b90569091b279a4724ae21421d3c00c19936fa
Edidiong Moses <edimosphy@gmail.com>
2025-08-22 22:39:41 +0100

    Update 1_Visualization.py

--- pages/1_Visualization.py
+++ pages/1_Visualization.py
@@ -60,7 +60,7 @@ for container in ax.containers:
 st.pyplot(plt)
 plt.clf()
 
-st.write("This bar chart shows the average Mean Nutrient Adequacy Ratio Index (MNARI) for each region in Ghana. Lower MNARI values indicate a higher nutrient gap.")
+st.write("This chart shows the average Mean Nutrient Adequacy Ratio Index (MNARI) for each region in Ghana. Higher MNARI values indicate a higher nutrient gap.")
 
 
 st.header("Mean Nutrient Adequacy Ratio Index by Commodity")
@@ -76,10 +76,87 @@ for container in ax.containers:
 st.pyplot(plt)
 plt.clf()
 
-st.write("This bar chart displays the average MNARI for different commodities. Commodities with lower MNARI might be less accessible or consumed in areas with high nutrient gaps.")
+st.write("This chart displays the average MNARI for different commodities. Commodities with higher MNARI might be less accessible or consumed in areas with high nutrient gaps due to many factors such as price and low yields.")
 
 
-st.header("Top 5 Commodities with the Most Significant Nutrient Gap")
+st.header("Price by Commodity")
+# Price Vs Commodity
+plt.figure(figsize= (15,8))
+ax = sns.barplot(data= nutrient_gap_original, x= "commodity", y= "price", errorbar=None)
+plt.xlabel("Commodity", fontsize= 12, fontweight= "bold")
+plt.ylabel("Price", fontsize= 12, fontweight= "bold")
+plt.title("Price by Commodity", fontsize= 14, fontweight= "bold")
+plt.xticks(rotation=90)
+# Add text labels on top of each bar
+for container in ax.containers:
+    ax.bar_label(container, fmt='%.1f')
+st.pyplot(plt)
+plt.clf()
+st.write("This chart displays the price of different commodities. Commodities with higher price might be less accessible or consumed in areas thus leading to high nutrient gaps and hunger.")
+
+
+st.header("Per Capita Food Consumption Index by Region")
+# Region Vs PCFCI
+plt.figure(figsize= (12,5))
+ax = sns.barplot(data= nutrient_gap_original, x ="region", y ="pcfci", errorbar= None)
+plt.title("Per Capita Food Consumption Index by Region", fontsize= 14, fontweight= "bold")
+plt.xlabel("Region", fontsize= 12, fontweight= "bold")
+plt.ylabel("Per Capita Food Consumption Index (PCFCI)", fontsize= 12, fontweight= "bold")
+plt.xticks(rotation=90)
+#Add text labels on top of each bar
+for container in ax.containers:
+  ax.bar_label(container, fmt= "%.2f")
+st.pyplot(plt)
+plt.clf()
+st.write("This chart displays the average amount of food available for consumption in a region. Lower PCFCI values indicates severe nutrient gaps in the region.")
+
+st.header("Vulnerability to Climate Change Index by Region")
+# Region Vs VCCI
+plt.figure(figsize=(12, 5))
+ax= sns.barplot(data= nutrient_gap_original, x= "region", y= "vcci", errorbar= None)
+plt.title("Vulnerability to Climate Change Index by Region", fontsize= 14, fontweight= "bold")
+plt.xlabel("Region", fontsize= 12, fontweight= "bold")
+plt.ylabel("Vulnerability to Climate Change Index (VCCI)", fontsize= 12, fontweight= "bold")
+plt.xticks(rotation=90)
+#Add text labels on top of each bar
+for container in ax.containers:
+  ax.bar_label(container, fmt= "%.2f")
+st.pyplot(plt)
+plt.clf()
+st.write("This chart displays effect of climate change. Higher VCCI leads to low agricultural production and yield thus severe nutrient gap")
+
+st.header("Per Capita Food Consumption Index by Commodity")
+# Commodity Vs PCFCI
+plt.figure(figsize= (15,8))
+ax = sns.barplot(data= nutrient_gap_original, x= "commodity", y= "pcfci", errorbar=None)
+plt.title("Per Capita Food Consumption Index by Commodity", fontsize= 14, fontweight= "bold")
+plt.xlabel("Commodity", fontsize= 12, fontweight= "bold")
+plt.ylabel("Per Capita Food Consumption Index (PCFCI)", fontsize= 12, fontweight= "bold")
+plt.xticks(rotation=90)
+# Add text labels on top of each bar
+for container in ax.containers:
+    ax.bar_label(container, fmt='%.2f')
+st.pyplot(plt)
+plt.clf()
+st.write("This chart displays the average amount of a particular food available for consumption. Lower PCFCI values indicates severe nutrient deficiency.")
+
+
+st.header("Vulnerability to Climate Change Index by Commodity")
+# Commodity Vs VCCI
+plt.figure(figsize= (15,8))
+ax = sns.barplot(data= nutrient_gap_original, x= "commodity", y= "vcci", errorbar=None)
+plt.title("Vulnerability to Climate Change Index by Commodity", fontsize= 14, fontweight= "bold")
+plt.xlabel("Commodity", fontsize= 12, fontweight= "bold")
+plt.ylabel("Vulnerability to Climate Change Index (VCCI)", fontsize= 12, fontweight= "bold")
+plt.xticks(rotation=90)
+# Add text labels on top of each bar
+for container in ax.containers:
+    ax.bar_label(container, fmt='%.2f')
+st.pyplot(plt)
+plt.clf()
+st.write("This chart displays the effect of climate change across agricultural production. Higher VCCI leads to low agricultural production and yield thus severe nutrient gap")
+
+st.header("Top 5 Commodities that is Highly Consumable Per Region")
 # Top 5 Commodity Vs Price Vs Region
 plt.figure(figsize= (15, 10))
 ax = sns.barplot(data=top_5_nutrient_gap, x="region", y="price", hue="commodity", errorbar=None)
@@ -95,10 +172,10 @@ for container in ax.containers:
 st.pyplot(plt)
 plt.clf()
 
-st.write("This bar chart illustrates the prices of the top 5 most significant nutrient-gap commodities across different regions. This helps to visualize regional price variations for key staples.")
+st.write("This chart illustrates the prices of the top 5 most consume commodities across different regions. This helps to visualize regional price variations for key staples foods.")
 
 
-st.header("5 Commodities with the Least Significant Nutrient Gap")
+st.header("5 Commodities that is Less Consumable Per Region")
 # Less 5 Commodity Vs Price Vs Region
 plt.figure(figsize= (15, 10))
 ax = sns.barplot(data=less_5_nutrient_gap, x="region", y="price", hue="commodity", errorbar=None)
@@ -114,7 +191,7 @@ for container in ax.containers:
 st.pyplot(plt)
 plt.clf()
 
-st.write("This chart shows the prices of the 5 least significant nutrient-gap commodities by region, which can provide insights into the cost of more common food sources.")
+st.write("This chart shows the prices of the 5 less consumable commodities by region, which can provide insights into the cost of food sources.")
 
 
 # Correlation Matrix
@@ -126,15 +203,14 @@ sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".1f")
 plt.title('Correlation Matrix')
 st.pyplot(plt)
 plt.clf()
-
 st.write("The correlation matrix shows the relationships between the numerical features and the one-hot encoded commodity features in the dataset.")
 
 st.header("Key Insight")
 st.write("""
-  - **Nutrient Gap Distribution:** The Mean Nutrient Adequacy Ratio Index (MNARI), used as the target variable, shows regional variations in nutrient adequacy. Some regions, particularly Eastern and Central, exhibit lower MNARI values (higher nutrient gaps), while Upper West and Upper East show higher MNARI values (lower nutrient gaps).
-  - **Commodity Consumption Patterns:** Staple crops like Maize, Yam, and Plantains are the most consumed commodities, while highly nutritious options such as Rice (Paddy), Meat (Chicken, Local), and Cowpeas have lower consumption rates.
+  - **Nutrient Gap Distribution:** The Mean Nutrient Adequacy Ratio Index (MNARI), used as the target variable, shows regional variations in nutrient adequacy. Some regions, particularly Eastern and Central, exhibit low MNARI values (lower nutrient gaps), while Upper West and Upper East show low MNARI values (severe nutrient gaps).
+  - **Commodity Consumption Patterns:** Staple crops like Maize, Yam, and Plantains are the most consumed commodities, while highly nutritious options such as Rice (Paddy), Meat (Chicken, Local), and Cowpeas have lower consumption rates due to price rate.
   - **Price and Nutrient Gaps:** Commodities with higher nutritional value tend to have higher prices, potentially limiting access for vulnerable populations and contributing to nutrient gaps.
-  - **Climate Change Vulnerability:** Regions with higher vulnerability to climate change, such as Northern and Upper West, appear to be associated with certain nutrient gap conditions. Climate conditions may impact agricultural production and food availability.
+  - **Climate Change Vulnerability:** Regions with higher vulnerability to climate change, such as Northern and Upper West, appear to be associated with certain nutrient gap conditions. Climate conditions may impact agricultural production and food availability thus leading to hunger.
 
 """)
 
